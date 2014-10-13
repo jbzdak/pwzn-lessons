312141ee7233d9e1ffe7fe0f616cd8e4d44c5454
Jacek Bzdak <jbzdak@gmail.com>
2014-10-13 23:31:58 +0200

    First version of Zaj2 notebooks

--- compile-notebook.sh
+++ compile-notebook.sh
@@ -8,15 +8,18 @@ function compile(){
 
     zaj=$1
     cd $SRC/$zaj
-    ipython nbconvert zaj1-blok*.ipynb --to rst
-    ipython nbconvert zaj1-blok*.ipynb --to html
-    ipython nbconvert zaj1-blok*.ipynb --to slides --reveal-prefix ../reveal.js
+    ipython nbconvert ${zaj}-blok*.ipynb --to rst
+    ipython nbconvert ${zaj}-blok*.ipynb --to html
+    ipython nbconvert ${zaj}-blok*.ipynb --to slides --reveal-prefix ../reveal.js
 
     #mkdir -p $CONTENT/$zaj
     mkdir -p $CONTENT/static/$zaj
 
     #cp *rst $CONTENT/$zaj
     cp *html $CONTENT/static/$zaj
+    cp -r ${zaj}*files $CONTENT/static/$zaj
 }
 
-compile zaj1
+#compile zaj1
+compile zaj2
+
